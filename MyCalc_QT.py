c44dd8cfba37a3054cf4c56abd719aa78b42d1c8
Nathan Brewer <brewer.nathant@gmail.com>
2021-01-12 19:18:25 -0600

    Tested and fixed is_prime

--- MyCalc_QT.py
+++ MyCalc_QT.py
@@ -238,20 +238,23 @@ class Page(QWidget):
         self.setWindowTitle("My First Qt Calculator")
 
 def is_prime(n):
-    if n==0 or n == 2 or n == 3: return True
-    if n < 2 or n%2 == 0: return False
-    if n < 9: return True
-    if n%3 == 0: return False
-    r = int(n**0.5)
-    f = 5
-    while f <= r:
-        if n%f == 0:
-            return False
-        if n%(f+2) == 0:
-            return False
+      """
+          Trial division, should be ok for numbers less than 20 digits if prime.
+      """
+      if n==0 or n == 2 or n == 3: return True
+      if n < 2 or n%2 == 0: return False
+      if n < 9: return True
+      if n%3 == 0: return False
+      r = int(n**0.5)
+      f = 5
+      while f <= r:
+            if n%f == 0:
+              return False
+            if n%(f+2) == 0:
+              return False
             f+=6
-        
-        return True
+            
+      return True
 
 if __name__ == "__main__":
     import sys
